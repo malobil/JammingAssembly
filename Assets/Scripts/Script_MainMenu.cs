44a5ad01a814231374e625c17fbe19b3954d3df4
Sébastien BREDA <seb.gx@hotmail.fr>
2019-11-09 20:05:15 +0100

    main menu

---
+++ Assets/Scripts/Script_MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_MainMenu : MonoBehaviour
+{
+    [Header("SceneManager")]
+
+    public string s_game_scene;
+
+    public void Awake()
+    {
+
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(s_game_scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
