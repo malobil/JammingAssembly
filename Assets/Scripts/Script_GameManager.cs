26616996c58b0c29131044ec85d4c9d0d5286545
Kevin_Ibanez <kevin.ibanezpro@gmail.com>
2019-11-09 12:46:06 +0100

    new version

--- Assets/Scripts/Script_GameManager.cs
+++ Assets/Scripts/Script_GameManager.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CellType {Empty, Normal, Rare, Bedrock}
+public enum CellType { Empty, Normal, Rare, Bedrock }
 
 public class Script_GameManager : MonoBehaviour
 {
-    public Vector2 gridSize = new Vector2(10,10) ;
+    public Vector2 gridSize = new Vector2(10, 10);
+    public int playersStartCells = 5;
     public float gridCellSize = 64f;
+    public int playerCount = 2;
 
     public GameObject prefabNormalRock;
+    public GameObject prefabGround;
     public GameObject prefabRareRock;
     public GameObject prefabBedRock;
+
+    public GameObject prefabPlayer;
+
 
     [Range(0, 100)]
     public int chanceRareRock = 35;
 
-    public List<Cell> cellList ;
+    private List<Grid> playersGrids = new List<Grid>();
+    private Vector2 playersStartCell;
 
     // Start is called before the first frame update
     void Start()
@@ -24,39 +31,69 @@ public class Script_GameManager : MonoBehaviour
         CreateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void CreateGrid()
     {
-        for(int x = 0; x < gridSize.x; x++)
+        playersStartCell = new Vector2(Mathf.FloorToInt(gridSize.x/2), Mathf.FloorToInt(gridSize.y/2));
+        GameObject gridBaseParent = new GameObject();
+        gridBaseParent.name = "PlayerGrid";
+        Grid newGrid = new Grid();
+
+        for (int x = 0; x < gridSize.x; x++)
         {
-            for(int y = 0; y < gridSize.y; y++)
+            for (int y = 0; y < gridSize.y; y++)
             {
                 GameObject newCellInst;
                 Cell newCell = new Cell();
                 newCell.cellPosition = new Vector2(x, y);
-                cellList.Add(newCell);
+                newCell.cellName = (x + "/" + y);
+                newGrid.gridCells.Add(newCell);
 
-                int rdm = Random.Range(1, 101);
-                
-                if(rdm <= chanceRareRock)
+
+                if (x >= playersStartCell.x - playersStartCells && x <= playersStartCell.x + playersStartCells
+                 && y >= playersStartCell.y - playersStartCells && y <= playersStartCell.y + playersStartCells)
                 {
-                    newCellInst = Instantiate(prefabRareRock, new Vector2(x * gridCellSize, y * gridCellSize), Quaternion.identity);
-                    newCell.cellType = CellType.Rare;
+                    newCell.cellType = CellType.Empty;
+                    newCellInst = Instantiate(prefabGround, new Vector2(x * gridCellSize, y * gridCellSize), Quaternion.identity);
                 }
                 else
                 {
-                    newCellInst = Instantiate(prefabNormalRock, new Vector2(x * gridCellSize, y * gridCellSize), Quaternion.identity);
-                    newCell.cellType = CellType.Normal;
-                }
+                    int rdm = Random.Range(1, 101);
 
-                newCell.cellName = (x + "/" + y);
+                    if (rdm <= chanceRareRock)
+                    {
+                        newCellInst = Instantiate(prefabRareRock, new Vector2(x * gridCellSize, y * gridCellSize), Quaternion.identity);
+                        newCell.cellType = CellType.Rare;
+                    }
+                    else
+                    {
+                        newCellInst = Instantiate(prefabNormalRock, new Vector2(x * gridCellSize, y * gridCellSize), Quaternion.identity);
+                        newCell.cellType = CellType.Normal;
+                    }
+                }
+                
                 newCellInst.name = newCell.cellName;
+                newCellInst.transform.SetParent(gridBaseParent.transform);
+                newCell.cellGameObject = newCellInst;
+
             }
+        }
+
+        playersGrids.Add(newGrid);
+
+        for (int i = 0; i < playerCount - 1; i++)
+        {
+            Instantiate(gridBaseParent, new Vector2(gridSize.x + 100f, 0), Quaternion.identity);
+            playersGrids.Add(playersGrids[0]);
+        }
+
+        SpawnPlayers();
+    }
+
+    void SpawnPlayers()
+    {
+        for(int i = 0; i < playerCount; i++)
+        {
+           // Instantiate(prefabPlayer,)
         }
     }
 }
@@ -66,5 +103,12 @@ public class Cell
 {
     public string cellName;
     public Vector2 cellPosition;
+    public GameObject cellGameObject ;
     public CellType cellType;
+}
+
+[System.Serializable]
+public class Grid
+{
+    public List<Cell> gridCells = new List<Cell>();
 }
