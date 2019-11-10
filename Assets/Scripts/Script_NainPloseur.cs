522dde9efc53a0fc48d3bbc1793ea417e95559f1
Kevin_Ibanez <kevin.ibanezpro@gmail.com>
2019-11-10 18:12:04 +0100

    Nain ploseur

--- Assets/Scripts/Script_NainPloseur.cs
+++ Assets/Scripts/Script_NainPloseur.cs
@@ -50,6 +50,57 @@ public class Script_NainPloseur : MonoBehaviour
     void Explosion()
     {
         Script_GameManager.Instance.SetCellType(playerDwarf, currentCell.cellPosition, CellType.Empty, null,null);
+        Script_GameManager.Instance.SpawnRock(playerDwarf, currentCell.cellPosition, CellType.Garbage);
+
+        Cell cellLeftPos = Script_GameManager.Instance.GetCellByPostion(playerDwarf, new Vector2(currentCell.cellPosition.x - 1, currentCell.cellPosition.y));
+        Cell cellRightPos = Script_GameManager.Instance.GetCellByPostion(playerDwarf, new Vector2(currentCell.cellPosition.x + 1, currentCell.cellPosition.y));
+        Cell cellUpPos = Script_GameManager.Instance.GetCellByPostion(playerDwarf, new Vector2(currentCell.cellPosition.x, currentCell.cellPosition.y + 1));
+        Cell cellDownPos = Script_GameManager.Instance.GetCellByPostion(playerDwarf, new Vector2(currentCell.cellPosition.x, currentCell.cellPosition.y - 1));
+        Cell cellDownRightPos = Script_GameManager.Instance.GetCellByPostion(playerDwarf, new Vector2(currentCell.cellPosition.x - 1, currentCell.cellPosition.y + 1));
+        Cell cellDownLeftPos = Script_GameManager.Instance.GetCellByPostion(playerDwarf, new Vector2(currentCell.cellPosition.x - 1, currentCell.cellPosition.y - 1));
+        Cell cellTopLeftPos = Script_GameManager.Instance.GetCellByPostion(playerDwarf, new Vector2(currentCell.cellPosition.x + 1, currentCell.cellPosition.y + 1));
+        Cell cellTopRightPos = Script_GameManager.Instance.GetCellByPostion(playerDwarf, new Vector2(currentCell.cellPosition.x + 1, currentCell.cellPosition.y - 1));
+
+        if(cellLeftPos.cellType == CellType.Empty)
+        {
+            Script_GameManager.Instance.SpawnRock(playerDwarf, cellLeftPos.cellPosition, CellType.Garbage);
+        }
+
+        if (cellRightPos.cellType == CellType.Empty)
+        {
+            Script_GameManager.Instance.SpawnRock(playerDwarf, cellRightPos.cellPosition, CellType.Garbage);
+        }
+
+        if (cellUpPos.cellType == CellType.Empty)
+        {
+            Script_GameManager.Instance.SpawnRock(playerDwarf, cellUpPos.cellPosition, CellType.Garbage);
+        }
+
+        if (cellDownPos.cellType == CellType.Empty)
+        {
+            Script_GameManager.Instance.SpawnRock(playerDwarf, cellDownPos.cellPosition, CellType.Garbage);
+        }
+
+        if (cellDownRightPos.cellType == CellType.Empty)
+        {
+            Script_GameManager.Instance.SpawnRock(playerDwarf, cellDownRightPos.cellPosition, CellType.Garbage);
+        }
+
+        if (cellDownLeftPos.cellType == CellType.Empty)
+        {
+            Script_GameManager.Instance.SpawnRock(playerDwarf, cellDownLeftPos.cellPosition, CellType.Garbage);
+        }
+
+        if (cellTopLeftPos.cellType == CellType.Empty)
+        {
+            Script_GameManager.Instance.SpawnRock(playerDwarf, cellTopLeftPos.cellPosition, CellType.Garbage);
+        }
+
+        if (cellTopRightPos.cellType == CellType.Empty)
+        {
+            Script_GameManager.Instance.SpawnRock(playerDwarf, cellTopRightPos.cellPosition, CellType.Garbage);
+        }
+
         Instantiate(prefabExplosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
