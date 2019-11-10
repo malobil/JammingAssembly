ff236d4de95ed26bcf67c715dbfbdd6cbf22c06e
MadFox <otaku.suprem@gmail.com>
2019-11-10 02:01:59 +0100

    menu manager

--- Assets/Scripts/Script_MainMenu.cs
+++ Assets/Scripts/Script_MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Script_MainMenu : MonoBehaviour
@@ -8,11 +9,35 @@ public class Script_MainMenu : MonoBehaviour
     [Header("SceneManager")]
 
     public string s_game_scene;
+	public GameObject defaultSelection;
 
     public void Awake()
     {
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
 
-    }
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+		{
+
+			CatchMouseClicks(defaultSelection);
+
+		}
+
+	}
+
+
+	public void CatchMouseClicks(GameObject setSelection)
+	{
+
+		EventSystem.current.SetSelectedGameObject(setSelection);
+
+	}
+
+
 
     public void Play()
     {
